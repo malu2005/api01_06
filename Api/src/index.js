cc28d88949346a87778ccbbf705c87472b3cb51b
malu2005 <malu2005>
2022-06-01 15:06:44 -0300

    oi

---
+++ Api/src/index.js
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import { con } from './repository/connection.js'
+
+import express from 'express'
+import cors from 'cors'
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+
+
+
+
+
+
+server.listen(process.env.PORT, () => console.log(`API Conectada Na Porta ${process.env.PORT}`));
